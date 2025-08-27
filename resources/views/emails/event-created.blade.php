92bda16c830656abd9cc682d4b164d1c6d3e92d4
Josiah Uma <umajosiah@gmail.com>
2025-08-28 00:47:51 +0100

    fixed Route [events.manage] not defined. error for email

--- resources/views/emails/event-created.blade.php
+++ resources/views/emails/event-created.blade.php
@@ -1,5 +1,5 @@
 <p>Hi {{ $event->organizer ?? $event->user->name ?? 'there' }},</p>
 <p>Your event <strong>{{ $event->name }}</strong> has been created.</p>
 <p>View it here: {{ route('events.show', $event->id) }}</p>
-<p>You can manage your event and tickets here: {{ route('events.manage', $event->id) }}</p>
+<p>You can manage your event and tickets here: {{ route('dashboard') }}</p>
 <p>Share your event and start selling tickets!</p>
