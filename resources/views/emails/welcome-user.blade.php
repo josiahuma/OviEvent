ea61f302aad49b95ebbf4ab1611126fec550f69f
Josiah Uma <umajosiah@gmail.com>
2025-08-28 00:02:20 +0100

    included create event link in welcome email

--- resources/views/emails/welcome-user.blade.php
+++ resources/views/emails/welcome-user.blade.php
@@ -1,3 +1,3 @@
 <p>Hi {{ $user->name ?? 'there' }},</p>
 <p>Welcome to <strong>Ovievent</strong>! Create, share and sell tickets for your events.</p>
-<p>Get started by creating your first event.</p>
+<p>Get started by creating your first event: {{ route('dashboard') }}</p>
