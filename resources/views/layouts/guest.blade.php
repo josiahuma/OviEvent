f9ac05ff1c31d05e081e41eb52c778de5ac17442
Josiah Uma <umajosiah@gmail.com>
2025-08-29 23:37:29 +0100

    enhanced login and register UI/UX

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,18 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
+        {{-- resources/views/layouts/guest.blade.php --}}
+        <div class="min-h-screen flex flex-col items-center justify-center bg-gray-50 px-4 py-8">
+            <a href="{{ route('homepage') }}" class="mb-6 inline-flex justify-center">
+                <img
+                    src="{{ asset('images/logo.png') }}"   {{-- update path if different --}}
+                    alt="{{ config('app.name', 'ovievent') }}"
+                    class="h-10 w-auto max-w-[180px] object-contain select-none"
+                    loading="lazy"
+                />
+            </a>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <div class="w-full sm:max-w-md">
                 {{ $slot }}
             </div>
         </div>
